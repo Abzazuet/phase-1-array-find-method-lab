7db128fbb823ea4aa057159c50be3db520929b06
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-27 14:44:45 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const didWin = element => element['result']=='W';
+
+function superbowlWin(arr){
+    let win = arr.find(didWin);
+    if(win==undefined){
+        return undefined;
+    }
+    else{
+        return win['year'];
+    }
+}
